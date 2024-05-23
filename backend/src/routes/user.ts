59937d63d393026a7cf118254d65e4573e5386cf
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-05-24 01:54:09 +0300

    req body validation with zod

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,7 @@
-import { Hono } from "hono"
 import { PrismaClient } from "@prisma/client/edge"
 import { withAccelerate } from "@prisma/extension-accelerate"
+import { signinInput, signupInput } from "@sub_1n/medium-common"
+import { Hono } from "hono"
 import { sign } from "hono/jwt"
 
 export const userRouter = new Hono<{
@@ -16,6 +17,11 @@ userRouter.post("/signup", async (c) => {
   }).$extends(withAccelerate())
 
   const body = await c.req.json()
+  const { success } = signupInput.safeParse(body)
+  if (!success) {
+    c.status(411)
+    return c.json({ message: "invalid input" })
+  }
 
   try {
     const user = await prisma.user.create({
@@ -40,6 +46,11 @@ userRouter.post("/signin", async (c) => {
   }).$extends(withAccelerate())
 
   const body = await c.req.json()
+  const { success } = signinInput.safeParse(body)
+  if (!success) {
+    c.status(411)
+    return c.json({ message: "invalid input" })
+  }
 
   const user = await prisma.user.findUnique({
     where: {
